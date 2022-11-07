7e4b311859e7e24aeb8905ca628470f70c9c1160
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2022-11-07 09:41:00 +0100

    Add email templates

--- src/templates/contentMails.template.js
+++ src/templates/contentMails.template.js
@@ -1,15 +1,8 @@
+import { englishTemplate } from './templates/englishTemplate.js'
+import { spanishTemplate } from './templates/spanishTemplate.js'
+
 export async function contact(username, content, language = 'es') {
   return language === 'es'
-    ? `
-  <h1>Contacto a Jero</h1>
-  <p>Gracias por contactar, ${username}.</p>
-  <p>Tu mensaje es el siguiente:</p>
-  <p>${content}</p>
-  <p>¡Gracias por contactar conmigo! Recibirás una respuesta en breve.</p>`
-    : `
-  <h1>Contact to Jero</h1>
-  <p>Thank you for contact me, ${username}.</p>
-  <p>Your message is:</p>
-  <p>${content}</p>
-  <p>Thank you for contact me! You will receive response soon.</p>`
+    ? spanishTemplate(username, content)
+    : englishTemplate(username, content)
 }
